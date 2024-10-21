d688a9c7d09e99ad5a695bc6f61a0f92c0b96fb4
Vamshigolla <vamshiofficial1493@gmail.com>
2024-10-21 16:57:02 +0530

    added new changes to the  ravi page

--- src/Pages/RaviLandingPage.js
+++ src/Pages/RaviLandingPage.js
@@ -1,8 +1,17 @@
 import React from "react";
 
 const RaviLandingPage = () => {
+    function myFunction(){
+        const text=document.getElementById("demo")
+        text.innerText="Vmshi"
+      }
+      
     return (
-        <span>This is Ravi Laniding Page</span>
+        <div>
+            <p id = "demo">Ravi Kumar DevOps</p>
+            <button type = "button" onClick={myFunction}>Try it</button>
+            {/* <a href="https://medium.com/@ravimanchi32/git-and-github-part-2-6c53ec889107" target="_blank"  rel="noreferrer">medium!</a> */}
+        </div>
     );
 };
-export default RaviLandingPage;
+export default RaviLandingPage; 
