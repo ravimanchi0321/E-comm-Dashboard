bedc6ba8c81012b9bfb758bed42bcfe878500de6
Priyakruth <Priyakruth027@gmail.com>
2024-10-22 17:44:50 +0530

    Girish Page has been add Task-1 Completed

---
+++ src/Components/TaskComponents/GirishTasks/GirishTask.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Girish1 = () => {
+    return (
+        <span>This is 1st Task page for Girish</span>
+        
+    );
+};
+
+export default Girish1;
--- src/Routes/RouteStack.js
+++ src/Routes/RouteStack.js
@@ -5,6 +5,7 @@ import RaviLandingPage from "../Pages/RaviLandingPage";
 import GirisLandingPage from "../Pages/GirishLandingPage";
 import VamshiLandingPage from "../Pages/VamshiLandingPage";
 import VamshiTask1 from "../Components/TaskComponents/VamshiTasks/VamshiTask1";
+import Girish1 from "../Components/TaskComponents/GirishTasks/GirishTask";
 
 const RouteStack = () => {
     return (
@@ -15,6 +16,7 @@ const RouteStack = () => {
                 <Route path="GirishPage" element={<GirisLandingPage />}></Route>
                 <Route path="VamshiPage" element={<VamshiLandingPage />}></Route>
                 <Route path="VamshiTask1" element={<VamshiTask1/>}></Route>
+                <Route path="GirishTask1" element={<Girish1/>}></Route>
             </Routes>
         </BrowserRouter>
     );
